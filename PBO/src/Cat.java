9bfb1b975ad1c874322d707067698ccb48145701
Muhammad Farhan Raflian N. P <145763402+Hannova753@users.noreply.github.com>
2024-06-24 21:45:31 +0700

    first commit

---
+++ PBO/src/Cat.java
@@ -0,0 +1,16 @@
+public class Cat {
+    public String nama = "Farhan";
+    public int umur = 2;
+    public String gender = "cowok";
+    public String makanan = "whiskas";
+    public String warna = "hitam putih";
+    public void iscathungry(){
+        System.out.println(this.nama + " is hungry");
+    }
+    public void iscatitred(){
+        System.out.println(this.nama + " go to sleep");
+    }
+    public void iscatangry(){
+        System.out.println(this.nama + " want to eat");
+    }
+}
